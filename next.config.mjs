f56143b8614e51939e132efd7a19513fd0b26dd8
Joshua Jessey <joshuajessey3@gmail.com>
2026-04-09 15:15:28 +0300

    Update next.config.mjs to conditionally apply API rewrites only in development

--- next.config.mjs
+++ next.config.mjs
@@ -9,14 +9,15 @@ const nextConfig = {
   turbopack: {
     root: process.cwd(),
   },
-  async rewrites() {
-    return [
-      {
-        source: '/api/:path*',
-        destination: 'http://127.0.0.1:8000/api/:path*',
-      },
-    ];
-  },
+}
+
+if (process.env.NODE_ENV === 'development') {
+  nextConfig.rewrites = async () => [
+    {
+      source: '/api/:path*',
+      destination: 'http://127.0.0.1:8000/api/:path*',
+    },
+  ];
 }
 
 export default nextConfig
